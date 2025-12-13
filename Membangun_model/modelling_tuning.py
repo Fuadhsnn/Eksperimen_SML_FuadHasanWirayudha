d961ffe2b22d1b07cfbd6ff2a385e84ae7af0fa1
Fuad Hasan <hasanyudha27@gmail.com>
2025-12-13 16:59:09 +0700

    Convert Outcome column to integer type

--- Membangun_model/modelling_tuning.py
+++ Membangun_model/modelling_tuning.py
@@ -42,7 +42,7 @@ DATA_PATH = os.path.join(BASE_DIR, "cleaned_pima_diabetes_fuad.csv")
 df = pd.read_csv(DATA_PATH)
 
 X = df.drop("Outcome", axis=1)
-y = df["Outcome"]
+y = df["Outcome"].astype(int) 
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
